07e20085a6d64ad753133436e5934d2f5ea514fe
retricsu <inneverland2013@gmail.com>
2023-01-16 11:54:11 +0800

    chore

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+require("hardhat-contract-sizer");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
